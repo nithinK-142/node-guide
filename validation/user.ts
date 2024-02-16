c0b347fde77aabdd9b308e0fab23225d029d3e88
nithinK-142 <nithin.sagar.359@gmail.com>
2024-02-16 15:48:59 +0530

    refactor: defining schema with shape.

--- validation/user.ts
+++ validation/user.ts
@@ -1,6 +1,6 @@
 import { ObjectSchema, object, string } from "yup";
 
-export const userSchema: ObjectSchema<UserSchemaType> = object({
+export const userSchema: ObjectSchema<UserSchemaType> = object().shape({
   name: string().min(4).required(),
   email: string().email("Enter a valid email!").required(),
   password: string().min(4).max(10).required(),
